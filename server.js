fc5d6007406b4533cbea11df03610010b54cbb20
Stacy Burris <stacy1burris@gmail.com>
2020-11-24 10:22:37 -0800

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const superagent = require('superagent');
 const methodOverride = require('method-override');
@@ -9,7 +10,7 @@ require('dotenv').config();
 const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3333;
 
-
+app.use(cors());
 app.use(methodOverride('_method'));
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
@@ -29,23 +30,30 @@ app.get('/search', getCity);
 
 function getCity(req, res) {
   let city = req.query.city;
-  console.log('are we finding the city', city);
+  //console.log('are we finding the city', city);
+  let exampleUrl = `https://kgsearch.googleapis.com/v1/entities:search?query=${city}&key=${GOOGLE_API_KEY}`;
   let url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city}&inputtype=textquery&key=${GOOGLE_API_KEY}`;
   let urlOne = `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJVTPokywQkFQRmtVEaUZlJRA&key=${GOOGLE_API_KEY}`;
 
   let urlThree = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=ATtYBwKHYJeS09ayLc48L31YwX56TmDxqiAiMDlOqdtOJRIVixCIIgrlmFcOJQOmz76kaxrxvOJ7pspBHtm7mdNrH6bh_2FB6FKno8UrcIghTTBh-6Iok-ccm6S7vpjvyAvSI33jOHCpt7JyEqhWZVsDKRbe_rr0zkdwc4s92FxAOgD3g6Io&key=${GOOGLE_API_KEY}`;
 
-  // superagent.get(url)
-  //   .then(data => {
-  //     console.log('what type of url', data.body);
 
-  //   })
-  superagent.get(urlOne)
+  superagent.get(exampleUrl)
     .then(data => {
-      console.log('what type of urlOne', data.body.result.url);
-      res.render('./pages/details', { cityDetails: data.body.result.url });
+      console.log('what type of example url', data.body);
 
     })
+
+
+
+
+
+  // superagent.get(urlOne)
+  //   .then(data => {
+  //     console.log('what type of urlOne', data.body.result.url);
+  //     res.render('./pages/details', { cityDetails: data.body.result.url });
+
+  //   })
   // superagent.get(urlThree)
   //   .then(data => {
   //     //console.log('urlThree data', data.body)
