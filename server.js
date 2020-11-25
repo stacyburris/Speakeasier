a8a8d1b0b9dcdea83c96bec84f522bfebd24f797
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2020-11-25 15:38:29 -0800

    saving locations, rendering to boarding

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.set('view engine', 'ejs');
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const GOOGLE_KN_API_KEY = process.env.GOOGLE_KN_API_KEY;
 ////////////////////////////////////////////////////TEST ROUTES/////////////////////////////////////////////////////////
-
+app.post('/boarding_pass', saveBoarding);
+app.get('/boarding', renderBoarding);
+// app.post('/stamped', saveStamped);
 app.get('/', (req, res) => {
   res.sendFile('./public/index.html');
 });
@@ -48,43 +50,58 @@ function getCity(req, res) {
     .then(() => {
       let urlPlaceId = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city}&inputtype=textquery&key=${GOOGLE_API_KEY}`;
       superagent.get(urlPlaceId)
-      .then(data => {
-        let pocket = data.body.candidates;
-        console.log('PLACE ID:', pocket);
-        let photoRefs = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${pocket[0].place_id}&key=${GOOGLE_API_KEY}`;
-        superagent.get(photoRefs)
-          .then(data => {
-            let photoReferenceArray = data.body.result.photos;
-            return photoReferenceArray.map(photos => photos.photo_reference)
-          })
-          .then(data => {
-            let array = data.map(photoArray => {
-              let url = `https://maps.googleapis.com/maps/api/place/photo?maxheight=200&photoreference=${photoArray}&key=${GOOGLE_API_KEY}`;
-              return url;
+        .then(data => {
+          let pocket = data.body.candidates;
+          console.log('PLACE ID:', pocket);
+          let photoRefs = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${pocket[0].place_id}&key=${GOOGLE_API_KEY}`;
+          superagent.get(photoRefs)
+            .then(data => {
+              let photoReferenceArray = data.body.result.photos;
+              return photoReferenceArray.map(photos => photos.photo_reference)
             })
-            return array;
-          })
-          .then(banana => {
-            let photoArray = [];
-            banana.forEach(apple => {
-              photoArray.push(superagent.get(apple));
-            })
-            Promise.all(photoArray)
-              .then(potatoes => {
-                obj.photo = potatoes;
-                res.render('./pages/details', { render: obj });
+            .then(data => {
+              let array = data.map(photoArray => {
+                let url = `https://maps.googleapis.com/maps/api/place/photo?maxheight=200&photoreference=${photoArray}&key=${GOOGLE_API_KEY}`;
+                return url;
               })
-              .catch(err => console.error(err));
-          })
-      })
+              return array;
+            })
+            .then(banana => {
+              let photoArray = [];
+              banana.forEach(apple => {
+                photoArray.push(superagent.get(apple));
+              })
+              Promise.all(photoArray)
+                .then(potatoes => {
+                  obj.photo = potatoes;
+                  res.render('./pages/details', { render: obj });
+                })
+                .catch(err => console.error(err));
+            })
+        })
     })
 }
 
 
+function saveBoarding(req, res) {
+  console.log('REQ TO SAVE:', req.body);
+  let { city_name, city_description, image_url } = req.body;
+  let SQL = 'INSERT INTO boarding (city_name, city_description, image_url) VALUES ($1,$2,$3) RETURNING *;';
+  let values = [city_name, city_description, image_url];
 
+  client.query(SQL, values)
+    .then(res.redirect('/boarding'))
+}
 
+function renderBoarding(req, res) {
+  let SQL = 'SELECT * FROM boarding;';
 
-
+  return client.query(SQL)
+    .then(dbInfo => {
+      res.render('./pages/boarding', { boardingData: dbInfo.rows })
+      console.log('DB INFO FOR RENDERING:', dbInfo.rows);
+    });
+}
 
 
 
@@ -105,6 +122,17 @@ client.connect()
   })
 client.on('error', err => console.err(err));
 
+
+
+
+
+// query
+// // SELECT * FROM TableOld
+// // WHERE []
+// query
+// // INSERT INTO TableNew
+// query
+// // DELETE FROM TableOld
 
 
 
--- speakeasier.sql
+++ speakeasier.sql
@@ -1,10 +1,22 @@
-DROP TABLE IF EXISTS places;
+DROP TABLE IF EXISTS boarding;
 
-CREATE TABLE places(
+CREATE TABLE boarding(
 id SERIAL PRIMARY KEY,
 city_name VARCHAR(255),
 city_description TEXT,
-city_url VARCHAR,
+image_url VARCHAR,
 packing_list TEXT,
 journal TEXT
 );
+
+DROP TABLE IF EXISTS stamped;
+
+CREATE TABLE stamped(
+id SERIAL PRIMARY KEY,
+city_name VARCHAR(255),
+city_description TEXT,
+image_url VARCHAR,
+packing_list TEXT,
+journal TEXT 
+);
+
