da0a21503e3defd4c96a11a1f646335a3490c809
Nathan Rhead Cox <nrc4@hotmail.com>
2020-11-24 18:20:36 -0800

    Pull photos from the google places API and render to the page.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const superagent = require('superagent');
 const methodOverride = require('method-override');
 const pg = require('pg');
+const { promiseImpl } = require('ejs');
 require('dotenv').config();
 const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3333;
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+const GOOGLE_KN_API_KEY = process.env.GOOGLE_KN_API_KEY;
 ////////////////////////////////////////////////////TEST ROUTES/////////////////////////////////////////////////////////
 
 app.get('/', (req, res) => {
@@ -26,55 +28,63 @@ app.get('/', (req, res) => {
 //////////////////////////////////////////////ROUTES/////////////////////////////////////////////////////////////////
 
 app.get('/search', getCity);
+// app.get('/search', getCountry);
 
 
 function getCity(req, res) {
+  let obj = {};
   let city = req.query.city;
-  //console.log('are we finding the city', city);
-  let exampleUrl = `https://kgsearch.googleapis.com/v1/entities:search?query=${city}&key=${GOOGLE_API_KEY}`;
-  let url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city}&inputtype=textquery&key=${GOOGLE_API_KEY}`;
+  
+  let urlPlaceId = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city}&inputtype=textquery&key=${GOOGLE_API_KEY}`;
+
   let urlOne = `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJVTPokywQkFQRmtVEaUZlJRA&key=${GOOGLE_API_KEY}`;
 
-  let urlThree = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=ATtYBwKHYJeS09ayLc48L31YwX56TmDxqiAiMDlOqdtOJRIVixCIIgrlmFcOJQOmz76kaxrxvOJ7pspBHtm7mdNrH6bh_2FB6FKno8UrcIghTTBh-6Iok-ccm6S7vpjvyAvSI33jOHCpt7JyEqhWZVsDKRbe_rr0zkdwc4s92FxAOgD3g6Io&key=${GOOGLE_API_KEY}`;
 
+  let googleKn = `https://kgsearch.googleapis.com/v1/entities:search?query=${city}&key=${GOOGLE_KN_API_KEY}`;
 
-  superagent.get(exampleUrl)
-    .then(data => {
-      console.log('what type of example url', data.body);
+    // superagent.get(googleKn)
+    // .then(data => {
+    //   // console.log('Google Knowledge', data);
+    //   // let array = [];
+    //   // data.body.itemListElement.map(results => {
+    //   //   array.push(results.result);
+    //   //   console.log(array);
+    //   // })
+    // })
 
-    })
-
-
-
-
-
-  // superagent.get(urlOne)
+  // superagent.get(url)
   //   .then(data => {
-  //     console.log('what type of urlOne', data.body.result.url);
-  //     res.render('./pages/details', { cityDetails: data.body.result.url });
-
+  //     console.log('what type of example url', data.body);
   //   })
-  // superagent.get(urlThree)
-  //   .then(data => {
-  //     //console.log('urlThree data', data.body)
-  //     res.render('./pages/details', { photoResult: data });
+  // obj.cityDetails = data.body.result.url; 
 
+  superagent.get(urlOne)
+    .then(data => {
+      console.log(data);
+      let photoReferenceArray = data.body.result.photos;
+      return photoReferenceArray.map(photos => photos.photo_reference)
+      })
+      .then(data => {
+          let array = data.map(photoArray => {
+            let url = `https://maps.googleapis.com/maps/api/place/photo?maxheight=200&photoreference=${photoArray}&key=${GOOGLE_API_KEY}`;
+            return url;
+          })
+          return array;
+        })
+        .then(banana => {
+         let photoArray = [];
+         banana.forEach(apple => {
+          photoArray.push(superagent.get(apple));
+        })
+        // console.log('PROMISE:', photoArray);
+        Promise.all(photoArray)
+          .then(potatoes => {
+            res.render('./pages/details', { render: potatoes });
 
-  //   });
-
-
+          })
+          .catch(err => console.error(err));
+     })
 }
-
-
-
-
-
-//result
-
-
-
-
-
 
 
 
